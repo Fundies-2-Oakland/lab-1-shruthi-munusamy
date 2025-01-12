5b6a6cb9359972b362f224c162dea896c810a29f
Shruthi Munusamy <munusamy.s@northeastern.edu>
2025-01-11 23:13:17 -0800

    lab1

---
+++ src/main/java/org/example/Vector3D.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Vector3D {
+    // QUESTION 1
+    private final double x;
+    private final double y;
+    private final double z;
+
+    public Vector3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    // QUESTION 2
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+}
